0f318f0f2eb04b4a3916be336d6e92b1fea92988
Brady <brady.peters@keyin.com>
2021-12-13 11:46:32 -0330

    Controller for Authentication

---
+++ src/main/java/com/keyin/AtlasSearch/Controller/AuthenticationController.java
@@ -0,0 +1,38 @@
+package com.keyin.AtlasSearch.Controller;
+import com.keyin.AtlasSearch.Web.User;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+@Service
+public class AuthenticationController {
+
+    @RequestMapping(value = { "/login" }, method = RequestMethod.GET)
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public ModelAndView register() {
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("register");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        return modelAndView;
+    }
+}
