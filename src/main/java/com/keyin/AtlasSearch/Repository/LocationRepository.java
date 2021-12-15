9439df0508bb862ca2b764027869ce19793bf22c
Mike Ryan <mike.ryan@keyin.com>
2021-12-15 11:11:52 -0330

    changed repo extended class to Jpa

--- src/main/java/com/keyin/AtlasSearch/Repository/LocationRepository.java
+++ src/main/java/com/keyin/AtlasSearch/Repository/LocationRepository.java
@@ -2,6 +2,7 @@ package com.keyin.AtlasSearch.Repository;
 
 import com.keyin.AtlasSearch.Model.Location;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface LocationRepository extends PagingAndSortingRepository<Location, Long> {
+public interface LocationRepository extends JpaRepository<Location, Long> {
 
      List<Location> findByCountry(@Param("Country") String Country);
 }
